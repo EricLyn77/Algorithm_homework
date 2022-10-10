eaafda59c3b4019442899c82d8d9c6b95cb5e8c2
EricLyn77 <ziruwang113@gmail.com>
2022-10-10 16:34:29 -0700

    homework5 problem1

--- Algorithm hw/Algorithm hw.cpp
+++ Algorithm hw/Algorithm hw.cpp
@@ -25,7 +25,7 @@ int main()
 	cout << endl;
 	system("pause");*/
 
-	vector<int> a{1,4,9,3,4,9,5,6,9,3,7,2};
+	/*vector<int> a{1,4,9,3,4,9,5,6,9,3,7,2};
 	merge_sort(a);
 	cout << "rerange array is:" << endl;
 	for (int i = 0; i < a.size(); i++)
@@ -33,8 +33,11 @@ int main()
 		cout << a[i] << " ";
 	}
 	cout << endl;
+	system("pause");*/
+
+	vector<int> A = { 6, 0, 2, 0, 1, 3, 4, 6, 1, 3, 2 };
+	reverse_counting_sort(find_vector_range(A), A);
 	system("pause");
-	
 }
 
 
@@ -211,5 +214,74 @@ void merge_sort(vector<int>& v)
 			cout << v[x] << " ";
 		}
 		cout << endl;
+	}
+}
+
+int find_vector_range(vector<int> A)
+{
+	int max = 0, min = 0;
+	for (int i = 0; i < A.size(); i++)
+	{
+		if (A[i] > max)
+		{
+			max = A[i];
+		}
+		if (A[i] < min)
+		{
+			min = A[i];
+		}
+	}
+	return max - min;
+}
+
+
+
+void reverse_counting_sort(int range, vector<int>A)
+{
+	int * C = new int[100];
+	int * B = new int[100]{0};
+
+	for (int i = 0; i < range; i++)
+	{
+		C[i] = 0;
+	}
+	for (int j = 0; j < A.size(); j++)
+	{
+		C[A[j]] = C[A[j]] + 1;
+	}
+	for (int i = range; i >= 0; i--)//count how many elements smaller than this value
+	{
+		C[i] = 0;
+		for (int j = 0; j < i; j++)
+		{
+			C[i] = C[i] + C[j];
+		}
+		cout << "there's " << C[i] << " elements smaller than " << i << endl;
+		for (int i = 0; i <= range; i++)
+		{
+			cout << C[i] << " ";
+		}
+		cout << endl;
+	}
+	cout << "The final C is:" << endl;
+	for (int i = 0; i <= range; i++)
+	{
+		cout << C[i] << " ";
+	}
+	cout << endl;
+	for (int j = 0; j < A.size(); j++)
+	{
+		B[C[A[j]]] = A[j];
+		C[A[j]] = C[A[j]] + 1;
+		cout << "the " << j << "th sort: ";
+		for (int a = 0; a < A.size(); a++)
+		{
+			cout << B[a] << " ";
+		}
+		cout << endl;
+	}
+	for (int a = 0; a<A.size(); a++)
+	{
+		cout << B[a] << " ";
 	}
 }
--- Algorithm hw/Algorithm hw.h
+++ Algorithm hw/Algorithm hw.h
@@ -7,3 +7,5 @@ using namespace std;
 int max_position(vector<int>, int, int, int&);
 void arrange_positive_negative(vector<int>&);
 void merge_sort(vector<int>&);
+int find_vector_range(vector<int>);
+void reverse_counting_sort(int, vector<int>);
